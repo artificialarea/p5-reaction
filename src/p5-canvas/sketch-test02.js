cd70dd8a47e636b19ea2025789f57b6d837d43ce
Sacha Sedriks <sachased@gmail.com>
2020-08-30 11:42:05 -0700

    Add simple animation with state

--- src/p5-canvas/sketch-test02.js
+++ src/p5-canvas/sketch-test02.js
@@ -6,9 +6,11 @@ export default class SketchTestTwo extends React.Component {
     constructor(props) {
         super(props)
         this.myRef = React.createRef()
+        this.inputRef = React.createRef()
         this.state = {
-            x: 100,
+            x: 20,
             y: 100,
+            w: 200,
             rgb: [255],
         }
     }
@@ -17,6 +19,9 @@ export default class SketchTestTwo extends React.Component {
         
         sketch.setup = () => {
             sketch.createCanvas(200, 200);
+
+            // disable draw() rerendering at 60fps (will control via React when to render)
+            sketch.noLoop();
         };
         
         sketch.draw = () => {
@@ -25,15 +30,46 @@ export default class SketchTestTwo extends React.Component {
             sketch.rect(this.state.x, this.state.y, 50, 50);
         };
     };
+
+    inputChanged() {
+        const val = this.inputRef.current.value;
+        console.log(val)
+        this.setState({
+            x: val
+        })
+    }
     
     componentDidMount() {
-        this.myP5 = new p5(this.s, this.myRef.current)
+        this.myP5 = new p5(this.s, this.myRef.current);
+
+        setTimeout(() => {
+            setInterval(() => {
+                this.setState({
+                    // x: this.state.x + 1
+                    x: this.state.x > this.state.w 
+                        ? -50
+                        : this.state.x + 1
+                })
+            }, 15)
+        }, 1000)
     }
 
     render() {
+        
+        if(this.myP5) {
+            this.myP5.redraw();
+        }
 
         return (
+            <div className="sketch">
+
             <div ref={this.myRef} />
+            <input 
+                ref={this.inputRef}
+                value={this.state.x}
+                onChange={this.inputChanged}
+            />
+            </div>
         );
     }
 }
